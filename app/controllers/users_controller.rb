435b1cf0b31fae84d4094627340ee5b32b2ec111
atsushi-niwayama <niwayamaatsushi@gmailcom>
2017-12-05 21:24:42 +0900

    編集アップデート
    編集アップデートが実装されました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
 
-	def edit 
-		@users = Users.find(params[:id])
-	end
+	# def edit 
+	# 	@users = Users.find(params[:id])
+	# end
 
-	def update
-		@user = User.find(params[:id])
-	end
+	# def update
+	# 	@user = User.find(params[:id])
+	# end
 
-
+    # private
+    # def private
 end
