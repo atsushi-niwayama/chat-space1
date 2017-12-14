3018c86aeaa8db1bc6a2545d9f08e7f1e674eefa
atsushi-niwayama <niwayamaatsushi@gmailcom>
2017-12-14 19:35:06 +0900

    7. 作成したグループをサイドバーに表示する
    done

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,31 @@
 class GroupsController < ApplicationController
+
+	def new
+		@group =Group.new
+	end
+
+	def create
+	 @group = Group.new(group_params)
+	 if @group.save
+	  redirect_to root_path
+	 else
+		render :new
+	 end
+	end
+
+	def edit
+		@group = Group.find(params[:id])
+	end	
+
+	def update
+		group = Group.find(params[:id])
+	end
+
+	private
+	
+
+	def group_params
+		params.require(:group).permit(:group_name, { :user_ids => []})
+	end
+	
 end
